ab310be94ad1d735cdf3526fdea4433d2c2af56b
michal ch <m0548550710@gmail.com>
2025-12-30 12:47:56 +0200

    change

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -3,15 +3,18 @@ using DalApi;
 using DalList;
 using DO;
 
+
 namespace DalTest
 {
     internal class Program
     { 
         static readonly IDal s_dal=new Dal.DalList();
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Initialization.Initialize(s_dal);
-
+           
         }
+
+
     }
 }
