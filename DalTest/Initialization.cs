7d2d497d1825bc0d770db5f1d1b58771832a8197
chanih <ch0527119706@gmail.com>
2025-12-30 12:07:55 +0200

    Create customer&sale db

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -9,14 +9,21 @@ public static class Initialization
 
     static void CreateCustomer()
     {
-       Customer customer = new Customer();
-       s_dal.Customer.Create(customer);
+        s_dal.Customer.Create(new Customer(1001, "Chana Levi", "Jerusalem", "052-7119706"));
+        s_dal.Customer.Create(new Customer(1002, "Miriam Cohen", "Tel Aviv", "054-3332211"));
+        s_dal.Customer.Create(new Customer(1003, "Yael Friedman", "Haifa", "050-9876543"));
+        s_dal.Customer.Create(new Customer(1004, "Rina Gold", "Be'er Sheva", "053-4455667"));
     }
+
     static void CreateSale()
     {
-        Sale sale = new Sale();
-        s_dal.Sale.Create(sale);
+        s_dal.Sale.Create(new Sale(1, 1001, 5, 49.90, true, DateTime.Now, DateTime.Now.AddDays(7)));
+        s_dal.Sale.Create(new Sale(2, 1002, 10, 39.90, true, DateTime.Now.AddDays(-3), DateTime.Now.AddDays(3)));
+        s_dal.Sale.Create(new Sale(3, 1003, 20, null, false, null, null));
+        s_dal.Sale.Create(new Sale(4, 1004, 3, 59.90, true, DateTime.Now, DateTime.Now.AddMonths(1)));
     }
+
+
     static void CreateProduct()
     {
         Product product = new Product();
--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,15 +1,16 @@
 ﻿using DO;
 using DalApi;
 using DalList;
+using Dal;
 
 namespace DalTest
 {
     internal class Program
     { 
-        static readonly IDal s_dal;
+        static readonly IDal s_dal=new Dal.DalList();
         static void Main(string[] args)
         {
-            //Initialization.Initialize(s_dal);
+            Initialization.Initialize(s_dal);
 
         }
     }
