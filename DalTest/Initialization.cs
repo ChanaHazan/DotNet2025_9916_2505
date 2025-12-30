0d0769c05f5813f50bd0823ff4f99c3bd5558a47
michal ch <m0548550710@gmail.com>
2025-12-30 12:15:13 +0200

    commit prudactDB

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -9,18 +9,22 @@ public static class Initialization
 
     static void CreateCustomer()
     {
-        Customer customer = new Customer();
-        s_dal.Customer.Create(new Customer());
-        s_dal.Customer.Create(new Customer());
-        s_dal.Customer.Create(new Customer());
-        s_dal.Customer.Create(new Customer());
+        s_dal.Customer.Create(new Customer(1001, "Chana Levi", "Jerusalem", "052-7119706"));
+        s_dal.Customer.Create(new Customer(1002, "Miriam Cohen", "Tel Aviv", "054-3332211"));
+        s_dal.Customer.Create(new Customer(1003, "Yael Friedman", "Haifa", "050-9876543"));
+        s_dal.Customer.Create(new Customer(1004, "Rina Gold", "Be'er Sheva", "053-4455667"));
 
     }
+
     static void CreateSale()
     {
-        Sale sale = new Sale();
-        s_dal.Sale.Create(sale);
+        s_dal.Sale.Create(new Sale(1, 1001, 5, 49.90, true, DateTime.Now, DateTime.Now.AddDays(7)));
+        s_dal.Sale.Create(new Sale(2, 1002, 10, 39.90, true, DateTime.Now.AddDays(-3), DateTime.Now.AddDays(3)));
+        s_dal.Sale.Create(new Sale(3, 1003, 20, null, false, null, null));
+        s_dal.Sale.Create(new Sale(4, 1004, 3, 59.90, true, DateTime.Now, DateTime.Now.AddMonths(1)));
     }
+
+
     static void CreateProduct()
     {
         s_dal.Product.Create(new Product(0, "BeefSteak", Categries.Meats, 120.5, 50));   
@@ -44,8 +48,9 @@ public static class Initialization
         s_dal.Product.Create(new Product(0, "Sardines", Categries.Fish, 35.0, 100));      
         // סרדינים
 
-
     }
+
+
     public static void Initialize(IDal dal)
     {
         s_dal = dal;
@@ -53,6 +58,5 @@ public static class Initialization
         CreateProduct();
         CreateSale();
     }
-
 
 }
