5dc9d43ed4b636b8719d7a039e7585d030a794b7
חנה חזן <ChanaHazan@smg.local>
2026-02-23 11:01:31 +0200

    fixed small problem

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -6,6 +6,7 @@ namespace DalTest;
 public static class Initialization
 {
     private static IDal s_dal;
+    static List<int> productIds = new List<int>();
 
     static void CreateCustomer()
     {
@@ -18,34 +19,34 @@ public static class Initialization
 
     static void CreateSale()
     {
-        s_dal.Sale.Create(new Sale(1, 1001, 5, 49.90, true, DateTime.Now, DateTime.Now.AddDays(7)));
-        s_dal.Sale.Create(new Sale(2, 1002, 10, 39.90, true, DateTime.Now.AddDays(-3), DateTime.Now.AddDays(3)));
-        s_dal.Sale.Create(new Sale(3, 1003, 20, null, false, null, null));
-        s_dal.Sale.Create(new Sale(4, 1004, 3, 59.90, true, DateTime.Now, DateTime.Now.AddMonths(1)));
+        s_dal.Sale.Create(new Sale(1, productIds[0], 5, 49.90, true, DateTime.Now, DateTime.Now.AddDays(7)));
+        s_dal.Sale.Create(new Sale(2, productIds[1], 10, 39.90, true, DateTime.Now.AddDays(-3), DateTime.Now.AddDays(3)));
+        s_dal.Sale.Create(new Sale(3, productIds[2], 20, null, false, null, null));
+        s_dal.Sale.Create(new Sale(4, productIds[3], 3, 59.90, true, DateTime.Now, DateTime.Now.AddMonths(1)));
     }
 
 
     static void CreateProduct()
     {
-        s_dal.Product.Create(new Product(0, "BeefSteak", Categories.Meats, 120.5, 50));   
+        productIds.Add(s_dal.Product.Create(new Product(0, "BeefSteak", Categories.Meats, 120.5, 50)));
         // סטייק בקר
-        s_dal.Product.Create(new Product(0, "GroundBeef", Categories.Meats, 65.0, 80));   
+        productIds.Add(s_dal.Product.Create(new Product(0, "GroundBeef", Categories.Meats, 65.0, 80)));
         // בשר בקר טחון
-        s_dal.Product.Create(new Product(0, "TurkeyBreast", Categories.Poultry, 82.0, 70));
+        productIds.Add(s_dal.Product.Create(new Product(0, "TurkeyBreast", Categories.Poultry, 82.0, 70)));
         // חזה הודו
-        s_dal.Product.Create(new Product(0, "TurkeySlices", Categories.Poultry, 80.7, 100));
+        productIds.Add(s_dal.Product.Create(new Product(0, "TurkeySlices", Categories.Poultry, 80.7, 100)));
         // פרוסות הודו
-        s_dal.Product.Create(new Product(0, "BlackPepper", Categories.Spices, 12.0, 200));   
+        productIds.Add(s_dal.Product.Create(new Product(0, "BlackPepper", Categories.Spices, 12.0, 200)));
         // פלפל שחור
-        s_dal.Product.Create(new Product(0, "Paprika", Categories.Spices, 10.0, 180));
+        productIds.Add(s_dal.Product.Create(new Product(0, "Paprika", Categories.Spices, 10.0, 180)));
         //פפריקה
-        s_dal.Product.Create(new Product(0, "FrozenBerries", Categories.Frozens, 25.0, 70));       
+        productIds.Add(s_dal.Product.Create(new Product(0, "FrozenBerries", Categories.Frozens, 25.0, 70)));
         // פירות יער קפואים
-        s_dal.Product.Create(new Product(0, "FrozenFalafel", Categories.Frozens, 20.0, 110));      
+        productIds.Add(s_dal.Product.Create(new Product(0, "FrozenFalafel", Categories.Frozens, 20.0, 110)));
         // פלאפל קפוא
-        s_dal.Product.Create(new Product(0, "Tilapia", Categories.Fish, 60.0, 80));        
+        productIds.Add(s_dal.Product.Create(new Product(0, "Tilapia", Categories.Fish, 60.0, 80)));
         // אמנון
-        s_dal.Product.Create(new Product(0, "Sardines", Categories.Fish, 35.0, 100));      
+        productIds.Add(s_dal.Product.Create(new Product(0, "Sardines", Categories.Fish, 35.0, 100)));      
         // סרדינים
     }
 
