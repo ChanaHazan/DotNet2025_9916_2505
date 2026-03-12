7dcc8a1ddb029bb9975f353d43b84fda87de29b1
chanih <ch0527119706@gmail.com>
2026-03-13 01:28:08 +0200

    step 9 (didnt finish)

--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -1,34 +1,47 @@
 ﻿
 namespace Dal;
 using DO;
+using System.Xml.Linq;
+
 internal static class DataSource
 {
     internal static List<Customer?> Customers = new List<Customer?>();
     internal static List<Product?> Products = new List<Product?>();
     internal static List<Sale?> Sales = new List<Sale?>();
 
+    
     internal static class Config
     {
-        internal const int firstIdProduct = 100000;
-        private static int productId=firstIdProduct;
-        private static int myVar;
+        const string path = @"xml\data-config.xml";
+        const string PRODUCTID = "productId";
+        const string SALEID = "saleId";
+        static XElement dataConfigXml = XElement.Load(path);
+        private static int ProductId= int.Parse(dataConfigXml.Element(PRODUCTID).Value);
+        private static int myVar; //מיותר?!
 
 
         public static int GetProductId
         {
             get
             {
-                return productId++;
+                ProductId++;
+                dataConfigXml.Element(PRODUCTID).SetValue(ProductId.ToString());
+                dataConfigXml.Save(path);
+                return ProductId;
             }
         }
-        internal const int firstIdSale = 100;
-        private static int saleId = firstIdSale;
+
+
+        private static int SaleId = int.Parse(dataConfigXml.Element(SALEID).Value);
 
         public static int GetSaleId
         {
             get
             {
-                return saleId++;
+                SaleId++;
+                dataConfigXml.Element(SALEID).SetValue(SaleId.ToString());
+                dataConfigXml.Save(path);
+                return SaleId;
             }
         }
     }
