91d22fe83703509e029e4b2e17ca0eb32994cf62
chanih <ch0527119706@gmail.com>
2026-04-27 19:48:12 +0300

    מוכן לשינויים של מיכל

--- UI/CustomerForm.cs
+++ UI/CustomerForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlApi;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,7 +11,7 @@ using System.Windows.Forms;
 
 namespace UI
 {
-    
+
 
     public partial class CustomerForm : Form
     {
@@ -18,12 +19,43 @@ namespace UI
         public CustomerForm(FormMode mode)
         {
             InitializeComponent();
+            SetupUI();
             CurrentMode = mode;
         }
 
+        private void SetupUI()
+        {
+            if (CurrentMode == FormMode.View)
+            {
+
+            }
+            if (CurrentMode == FormMode.Create)
+            {
+
+            }
+            if (CurrentMode == FormMode.ViewAll)
+            {
+
+            }
+            if (CurrentMode == FormMode.Update)
+            {
+
+            }
+            if (CurrentMode == FormMode.Delete)
+            {
+
+            }
+        }
         private void CustomerForm_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void backToLastForm_Click(object sender, EventArgs e)
+        {
+            ManagerForm manager = new ManagerForm();
+            manager.Show();
+            this.Hide();
         }
     }
 }
