4c9090e102199c38583074e74dab5c78104224a3
chanih <ch0527119706@gmail.com>
2026-05-03 02:18:41 +0300

    finish cashierForm

--- UI/CashierForm.Designer.cs
+++ UI/CashierForm.Designer.cs
@@ -35,18 +35,25 @@
             label1 = new Label();
             label2 = new Label();
             dataGridViewOrder = new DataGridView();
+            DeleteColumn = new DataGridViewButtonColumn();
             productID = new DataGridViewTextBoxColumn();
             productName = new DataGridViewTextBoxColumn();
             amount = new DataGridViewTextBoxColumn();
+            MinusColumn = new DataGridViewButtonColumn();
+            PlusColumn = new DataGridViewButtonColumn();
+            statusStrip1 = new StatusStrip();
+            lblFinalSum = new ToolStripStatusLabel();
+            btnDoOrder = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridViewOrder).BeginInit();
+            statusStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // backToMainForm
             // 
-            backToMainForm.Location = new Point(883, 652);
-            backToMainForm.Margin = new Padding(1);
+            backToMainForm.Location = new Point(2208, 1565);
+            backToMainForm.Margin = new Padding(2);
             backToMainForm.Name = "backToMainForm";
-            backToMainForm.Size = new Size(181, 29);
+            backToMainForm.Size = new Size(452, 70);
             backToMainForm.TabIndex = 1;
             backToMainForm.Text = "חזרה לדף הראשי";
             backToMainForm.UseVisualStyleBackColor = true;
@@ -54,56 +61,77 @@
             // 
             // toAddProduct
             // 
-            toAddProduct.Location = new Point(748, 220);
+            toAddProduct.Location = new Point(1870, 528);
+            toAddProduct.Margin = new Padding(8, 7, 8, 7);
             toAddProduct.Name = "toAddProduct";
-            toAddProduct.Size = new Size(147, 29);
+            toAddProduct.Size = new Size(368, 70);
             toAddProduct.TabIndex = 3;
             toAddProduct.Text = "להוספת מוצר";
             toAddProduct.UseVisualStyleBackColor = true;
+            toAddProduct.Click += toAddProduct_Click;
             // 
             // toChooseProduct
             // 
+            toChooseProduct.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            toChooseProduct.AutoCompleteSource = AutoCompleteSource.ListItems;
             toChooseProduct.FormattingEnabled = true;
-            toChooseProduct.Location = new Point(857, 154);
+            toChooseProduct.Location = new Point(2142, 370);
+            toChooseProduct.Margin = new Padding(8, 7, 8, 7);
             toChooseProduct.Name = "toChooseProduct";
-            toChooseProduct.Size = new Size(151, 28);
+            toChooseProduct.Size = new Size(372, 56);
             toChooseProduct.TabIndex = 4;
             // 
             // codeToAddProduct
             // 
-            codeToAddProduct.Location = new Point(677, 154);
+            codeToAddProduct.Location = new Point(1692, 370);
+            codeToAddProduct.Margin = new Padding(8, 7, 8, 7);
             codeToAddProduct.Name = "codeToAddProduct";
-            codeToAddProduct.Size = new Size(125, 27);
+            codeToAddProduct.Size = new Size(306, 55);
             codeToAddProduct.TabIndex = 5;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(686, 106);
+            label1.Location = new Point(1715, 254);
+            label1.Margin = new Padding(8, 0, 8, 0);
             label1.Name = "label1";
-            label1.Size = new Size(103, 20);
+            label1.Size = new Size(248, 48);
             label1.TabIndex = 6;
             label1.Text = "הכנס קוד מוצר";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(883, 106);
+            label2.Location = new Point(2208, 254);
+            label2.Margin = new Padding(8, 0, 8, 0);
             label2.Name = "label2";
-            label2.Size = new Size(94, 20);
+            label2.Size = new Size(225, 48);
             label2.TabIndex = 7;
             label2.Text = "לבחירת מוצר";
             // 
             // dataGridViewOrder
             // 
             dataGridViewOrder.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewOrder.Columns.AddRange(new DataGridViewColumn[] { productID, productName, amount });
-            dataGridViewOrder.Location = new Point(12, 46);
+            dataGridViewOrder.Columns.AddRange(new DataGridViewColumn[] { DeleteColumn, productID, productName, amount, MinusColumn, PlusColumn });
+            dataGridViewOrder.Location = new Point(30, 110);
+            dataGridViewOrder.Margin = new Padding(8, 7, 8, 7);
             dataGridViewOrder.Name = "dataGridViewOrder";
             dataGridViewOrder.RowHeadersWidth = 51;
-            dataGridViewOrder.Size = new Size(650, 621);
+            dataGridViewOrder.Size = new Size(1625, 1490);
             dataGridViewOrder.TabIndex = 8;
             dataGridViewOrder.CellContentClick += dataGridViewOrder_CellContentClick;
+            dataGridViewOrder.CellValidating += dataGridViewOrder_CellValidating;
+            dataGridViewOrder.CellValueChanged += dataGridViewOrder_CellValueChanged;
+            dataGridViewOrder.CurrentCellDirtyStateChanged += dataGridViewOrder_CurrentCellDirtyStateChanged;
+            // 
+            // DeleteColumn
+            // 
+            DeleteColumn.HeaderText = "";
+            DeleteColumn.MinimumWidth = 15;
+            DeleteColumn.Name = "DeleteColumn";
+            DeleteColumn.Text = "X";
+            DeleteColumn.UseColumnTextForButtonValue = true;
+            DeleteColumn.Width = 85;
             // 
             // productID
             // 
@@ -128,15 +156,61 @@
             amount.HeaderText = "כמות";
             amount.MinimumWidth = 6;
             amount.Name = "amount";
-            amount.ReadOnly = true;
             amount.Visible = false;
             amount.Width = 125;
             // 
+            // MinusColumn
+            // 
+            MinusColumn.HeaderText = "";
+            MinusColumn.MinimumWidth = 15;
+            MinusColumn.Name = "MinusColumn";
+            MinusColumn.Text = "-";
+            MinusColumn.UseColumnTextForButtonValue = true;
+            MinusColumn.Width = 80;
+            // 
+            // PlusColumn
+            // 
+            PlusColumn.HeaderText = "";
+            PlusColumn.MinimumWidth = 15;
+            PlusColumn.Name = "PlusColumn";
+            PlusColumn.Text = "+";
+            PlusColumn.UseColumnTextForButtonValue = true;
+            PlusColumn.Width = 80;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.ImageScalingSize = new Size(48, 48);
+            statusStrip1.Items.AddRange(new ToolStripItem[] { lblFinalSum });
+            statusStrip1.Location = new Point(0, 1680);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(2715, 22);
+            statusStrip1.TabIndex = 9;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // lblFinalSum
+            // 
+            lblFinalSum.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblFinalSum.Name = "lblFinalSum";
+            lblFinalSum.Size = new Size(0, 7);
+            // 
+            // btnDoOrder
+            // 
+            btnDoOrder.Location = new Point(1740, 752);
+            btnDoOrder.Margin = new Padding(8, 7, 8, 7);
+            btnDoOrder.Name = "btnDoOrder";
+            btnDoOrder.Size = new Size(604, 202);
+            btnDoOrder.TabIndex = 10;
+            btnDoOrder.Text = "לביצוע הזמנה";
+            btnDoOrder.UseVisualStyleBackColor = true;
+            btnDoOrder.Click += btnDoOrder_Click;
+            // 
             // CashierForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(20F, 48F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1086, 709);
+            ClientSize = new Size(2715, 1702);
+            Controls.Add(btnDoOrder);
+            Controls.Add(statusStrip1);
             Controls.Add(dataGridViewOrder);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -144,12 +218,14 @@
             Controls.Add(toChooseProduct);
             Controls.Add(toAddProduct);
             Controls.Add(backToMainForm);
-            Margin = new Padding(1);
+            Margin = new Padding(2);
             Name = "CashierForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "CashierForm";
             Load += CashierForm_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridViewOrder).EndInit();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -163,8 +239,14 @@
         private Label label1;
         private Label label2;
         private DataGridView dataGridViewOrder;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel lblFinalSum;
+        private DataGridViewButtonColumn DeleteColumn;
         private DataGridViewTextBoxColumn productID;
         private DataGridViewTextBoxColumn productName;
         private DataGridViewTextBoxColumn amount;
+        private DataGridViewButtonColumn MinusColumn;
+        private DataGridViewButtonColumn PlusColumn;
+        private Button btnDoOrder;
     }
 }
--- UI/CashierForm.cs
+++ UI/CashierForm.cs
@@ -12,6 +12,8 @@ namespace UI
 {
     public partial class CashierForm : Form
     {
+        BindingList<BO.ProductInOrder> cartList = new BindingList<BO.ProductInOrder>();
+        BO.Order currentOrder = new BO.Order { ProductInOrder = new List<BO.ProductInOrder>() };
         private BlApi.IBl _bl = BlApi.Factory.Get();
         public CashierForm()
         {
@@ -27,17 +29,198 @@ namespace UI
 
         private void CashierForm_Load(object sender, EventArgs e)
         {
-
+            dataGridViewOrder.DataSource = cartList;
+            dataGridViewOrder.AllowUserToAddRows = false;
+            var allProducts = _bl.Product.ReadAll();
+            toChooseProduct.DataSource = allProducts.ToList();
+            toChooseProduct.DisplayMember = "ProductName";
+            toChooseProduct.ValueMember = "Id";
         }
 
         private void dataGridViewOrder_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            if (e.RowIndex < 0) return;
 
+            var item = cartList[e.RowIndex];
+            string columnName = dataGridViewOrder.Columns[e.ColumnIndex].Name;
+            if (columnName == "PlusColumn")
+            {
+                RefreshRowData(e.RowIndex, 1);
+            }
+            else if (columnName == "MinusColumn")
+            {
+                if (item.Amount > 1)
+                {
+                    RefreshRowData(e.RowIndex, -1);
+                }
+            }
+            else if (columnName == "DeleteColumn")
+            {
+                cartList.RemoveAt(e.RowIndex);
+                UpdateFinalTotal();
+            }
         }
 
-        private void toRemoveProduct_Click(object sender, EventArgs e)
+        // פונקציית עזר קטנה כדי לא לחזור על קוד
+        private void RefreshRowData(int rowIndex, int amountToAdd)
         {
+            var item = cartList[rowIndex];
+            try
+            {
+                _bl.Order.AddProductToOrder(currentOrder, item.ProductId, amountToAdd);
+                dataGridViewOrder.InvalidateRow(rowIndex);
+                UpdateFinalTotal();
+            }
+            catch (BO.BLThereIsNotEnoughInStock ex)
+            {
+                MessageBox.Show(ex.Message, "מחסור במלאי", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                dataGridViewOrder.Refresh();
+            }
+        }
 
+        private void dataGridViewOrder_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && dataGridViewOrder.Columns[e.ColumnIndex].Name == "Amount")
+            {
+                var item = cartList[e.RowIndex];
+
+                try
+                {
+                    var product = _bl.Product.Read(item.ProductId);
+
+                    if (item.Amount > product.Stock)
+                    {
+                        throw new BO.BLThereIsNotEnoughInStock($"אין מספיק במלאי. קיים רק {product.Stock} יחידות.");
+                    }
+
+                    _bl.Order.SearchSaleForProduct(item, true);
+                    _bl.Order.CalcTotalPriceForProduct(item);
+
+                    dataGridViewOrder.InvalidateRow(e.RowIndex);
+                    UpdateFinalTotal();
+                }
+                catch (BO.BLThereIsNotEnoughInStock ex)
+                {
+                    MessageBox.Show(ex.Message, "שגיאת מלאי", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                    item.Amount = 1;
+                    _bl.Order.CalcTotalPriceForProduct(item);
+                    dataGridViewOrder.Refresh();
+                    UpdateFinalTotal();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("שגיאה בעדכון הכמות: " + ex.Message);
+                }
+            }
+        }
+        private void dataGridViewOrder_CurrentCellDirtyStateChanged(object sender, EventArgs e)
+        {
+            if (dataGridViewOrder.IsCurrentCellDirty)
+            {
+                dataGridViewOrder.CommitEdit(DataGridViewDataErrorContexts.Commit);
+            }
+        }
+
+        private void dataGridViewOrder_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            // בודקים אם השינוי הוא בעמודת הכמות
+            if (dataGridViewOrder.Columns[e.ColumnIndex].Name == "Amount")
+            {
+                int newAmount;
+                // 1. בדיקה שהוזן מספר תקין
+                if (!int.TryParse(e.FormattedValue.ToString(), out newAmount) || newAmount <= 0)
+                {
+                    MessageBox.Show("נא להזין כמות תקינה גדולה מ-0.");
+                    dataGridViewOrder.CancelEdit(); // מבטל את ההקלדה הנוכחית
+                    cartList[e.RowIndex].Amount = 1; // מחזיר ל-1
+                    return;
+                }
+
+                // 2. בדיקת מלאי מול ה-BL
+                var item = cartList[e.RowIndex];
+                var product = _bl.Product.Read(item.ProductId);
+
+                if (newAmount > product.Stock)
+                {
+                    MessageBox.Show($"טעות! אין מספיק במלאי. קיים רק {product.Stock} יחידות.", "חריגה מהמלאי");
+                    dataGridViewOrder.CancelEdit();
+                    item.Amount = 1;
+                    _bl.Order.CalcTotalPriceForProduct(item);
+                    UpdateFinalTotal();
+                }
+            }
+        }
+        private void toAddProduct_Click(object sender, EventArgs e)
+        {
+            int productId = 0;
+            if (!string.IsNullOrWhiteSpace(codeToAddProduct.Text) && int.TryParse(codeToAddProduct.Text, out int id))
+            {
+                productId = id;
+            }
+            else if (toChooseProduct.SelectedValue is int selectedId)
+            {
+                productId = selectedId;
+            }
+
+            if (productId > 0)
+            {
+                try
+                {
+                    var appliedSales = _bl.Order.AddProductToOrder(currentOrder, productId, 1);
+
+                    cartList.Clear();
+                    foreach (var item in currentOrder.ProductInOrder)
+                    {
+                        cartList.Add(item);
+                    }
+
+                    codeToAddProduct.Clear();
+                    toChooseProduct.SelectedIndex = -1;
+                    UpdateFinalTotal();
+                    dataGridViewOrder.Refresh();
+                }
+                catch (BO.BLThereIsNotEnoughInStock ex)
+                {
+                    MessageBox.Show(ex.Message, "מחסור במלאי", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("שגיאה: " + ex.Message);
+                }
+            }
+        }
+
+        private void UpdateFinalTotal()
+        {
+            double total = cartList.Sum(x => x.TotalPrice);
+            lblFinalSum.Text = $"סה\"כ לתשלום: {total:N2} ₪";
+        }
+
+        private void btnDoOrder_Click(object sender, EventArgs e)
+        {
+            
+                if (!cartList.Any())
+                {
+                    MessageBox.Show("הסל ריק!");
+                    return;
+                }
+
+                try
+                {
+                    _bl.Order.DoOrder(currentOrder);
+
+                    MessageBox.Show("ההזמנה בוצעה בהצלחה! המלאי עודכן במערכת.");
+
+                    currentOrder = new BO.Order { ProductInOrder = new List<BO.ProductInOrder>() };
+                    cartList.Clear();
+                    UpdateFinalTotal();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"שגיאה בביצוע ההזמנה: {ex.Message}");
+                }
+            
         }
     }
 }
